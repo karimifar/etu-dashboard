9bf8c7f4e073eb6c2195f3eb287d8acb3b8171c7
Karimifar <mkarimifar@utlocaldminsPro.utsystem.local>
2018-09-05 16:07:59 -0500

    added actual data

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+//The order in the array:
+// 0- 'UT Health at Houston'
+// 1- 'UT Health Northeast at Tyler'
+// 2- 'UT Health Science Center at San Antonio'
+// 3- 'UT MD Anderson Cancer Center'
+// 4- 'UT Medical Branch at Galveston'
+// 5- 'UT Southwestern Medical Center'
+// 6- 'UT Arlington'
+// 7- 'UT Austin'
+// 8- 'UT Dallas'
+// 9- 'UT El Paso'
+// 10- 'UT Permian Basin'
+// 11- 'UT Rio Grande Valley'
+// 12- 'UT San Antonio'
+// 13- 'UT Tyler'
+// 14- 'UT System Administration'
+
+//first digit= year (2016=1, 2017=2, 2018=3 0="none")
+//second digit= status: 1=check, 2= flag, 3= X 4="through research trials only" 0="none")
 var data={
     Prevention:{
         'Education Brochures': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
@@ -8,25 +27,25 @@ var data={
         'Hosting On-Campus Events': [33,33,31,12,11,31,11,11,12,11,11,12,11,33,33]
     },
     Policy:{
-        '100% Smoke-Free': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        '100% Tobacco-Free': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Includes ENDS and Other Tobacco Products (hookah, etc.)': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Policy Communication (signage)': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Enforcement Procedures': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Tobacco‑Free Hiring': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11]
+        '100% Smoke-Free': [11,11,11,11,11,11,11,11,31,11,21,11,11,21,11],
+        '100% Tobacco-Free': [11,11,11,11,11,11,11,11,31,11,21,11,11,21,11],
+        'Includes ENDS and Other Tobacco Products (hookah, etc.)': [11,11,11,11,11,11,11,21,12,11,21,11,11,21,11],
+        'Policy Communication (signage)': [11,12,11,11,11,11,11,11,31,11,21,12,11,21,11],
+        'Enforcement Procedures': [12,12,31,11,11,11,31,11,31,21,22,12,12,12,12],
+        'Tobacco‑Free Hiring': [13,12,13,11,13,12,13,13,13,13,13,13,13,13,13]
     },
     Cessation:{
-        'Tobacco Cessation Program Offered': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Individual Counseling Offered': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Group Counseling Offered': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Offered to Employee': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        "Offered to Employee's Dependents": [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Offered to Patients (If Applicatble)': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Available to Community': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Located On Campus': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Located Off-Site (Other Hospitals) (If Applicable)': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Telehealth': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Communication, Marketing of the Program': [11,11,11,13,11,11,11,11,22,11,32,31,11,12,11]
+        'Tobacco Cessation Program Offered': [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+        'Individual Counseling Offered': [11,33,11,33,33,11,21,11,12,11,12,12,11,11,11],
+        'Group Counseling Offered': [31,11,11,11,33,11,21,11,12,11,12,12,21,12,12],
+        'Offered to Employee': [11,11,11,11,33,11,11,11,11,11,11,11,11,11,11],
+        "Offered to Employee's Dependents": [12,11,11,11,33,12,12,12,12,12,21,11,11,11,12],
+        'Offered to Patients (If Applicatble)': [11,11,11,11,33,11,40,40,40,40,40,40,40,40,40],
+        'Available to Community': [13,13,31,44,13,31,13,44,13,21,13,13,13,13,13],
+        'Located On Campus': [11,11,11,11,33,11,31,33,31,11,33,33,11,21,33],
+        'Located Off-Site (Other Hospitals) (If Applicable)': [11,11,11,11,40,11,40,40,40,40,40,40,40,40,40],
+        'Telehealth': [12,33,12,11,33,12,12,12,12,21,12,12,12,12,12],
+        'Communication, Marketing of the Program': [11,33,11,11,33,12,12,11,12,11,12,12,11,12,11]
     }
 }
 
@@ -74,6 +93,13 @@ function printRowData(array, rowNum){
                 break;
             case '3':
                 td = "<td class='text-center'><img src='./img/x.png'></td>";
+                break;
+            case '4':
+                console.log("HERE")
+                td = "<td class='text-center'>Through research trials only</td>";
+                break;
+            case '0':
+                td = "<td class='text-center'></td>";
                 break;
         }
         $('#row-'+rowNum).append(td);
@@ -131,22 +157,4 @@ function switchColor(a, b, cell){
         $("button."+cell).css("background-color", "rgb(255, 255, 255)")
     }
 }
-//The order in the array:
-// 0- 'UT Health at Houston'
-// 1- 'UT Health Northeast at Tyler'
-// 2- 'UT Health Science Center at San Antonio'
-// 3- 'UT MD Anderson Cancer Center'
-// 4- 'UT Medical Branch at Galveston'
-// 5- 'UT Southwestern Medical Center'
-// 6- 'UT Arlington'
-// 7- 'UT Austin'
-// 8- 'UT Dallas'
-// 9- 'UT El Paso'
-// 10- 'UT Permian Basin'
-// 11- 'UT Rio Grande Valley'
-// 12- 'UT San Antonio'
-// 13- 'UT Tyler'
-// 14- 'UT System Administration'
 
-//first digit= year (2016=1, 2017=2, 2018=3)
-//second digit= status: 1=check, 2= flag, 3= X)
