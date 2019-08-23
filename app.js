aa4c84a371533a60437c5ac231f4e860b1839cd7
Karimifar <em.karimifar@gmail.com>
2019-08-23 09:18:44 -0500

    updates

--- app.js
+++ app.js
@@ -20,32 +20,32 @@
 var data={
     Prevention:{
         'Education Brochures': [11,11,11,11,11,11,11,11,22,11,12,31,11,12,11],
-        'Technology Based Program': [11,12,12,31,33,42,11,11,42,11,33,42,11,12,11],
-        'Available for Employees': [11,11,11,11,11,11,11,11,33,11,12,42,11,12,11],
-        "Available for Employees' Dependents": [12,12,11,11,11,41,12,11,33,12,12,42,11,12,11],
-        'Available for Community': [11,11,11,11,11,11,12,12,12,21,12,42,32,33,11],
-        'Hosting On-Campus Events': [33,33,31,12,11,31,11,11,41,11,11,42,11,33,33]
+        'Technology Based Program': [11,12,12,31,33,42,11,11,42,11,33,41,11,12,11],
+        'Available for Employees': [11,11,11,11,11,11,11,11,33,11,12,41,11,12,11],
+        "Available for Employees' Dependents": [12,12,11,11,11,41,12,11,33,12,12,41,11,12,11],
+        'Available for Community': [11,11,11,11,11,11,12,12,12,21,12,41,32,33,11],
+        'Hosting On-Campus Events': [33,33,31,12,11,31,11,11,41,11,11,41,11,42,33]
     },
     Policy:{
         '100% Smoke-Free': [11,11,11,11,11,11,11,11,31,11,21,11,11,21,11],
         '100% Tobacco-Free': [11,11,11,11,11,11,11,11,31,11,21,11,11,21,11],
         'Includes ENDS and Other Tobacco Products (hookah, etc.)': [11,11,11,11,11,11,11,21,41,11,21,11,11,21,11],
-        'Policy Communication (signage)': [11,12,11,11,11,11,11,11,31,11,21,42,11,21,11],
+        'Policy Communication (signage)': [11,12,11,11,11,11,11,11,31,11,21,41,11,42,11],
         'Enforcement Procedures': [31,12,31,11,11,11,31,11,31,21,22,42,12,12,12],
         'Tobacco‑Free Hiring': [13,41,13,11,13,12,13,13,13,13,13,13,13,13,13]
     },
     Cessation:{
         'Tobacco Cessation Program Offered': [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
-        'Individual Counseling Offered': [11,33,11,33,33,11,21,11,41,11,12,42,11,11,11],
-        'Group Counseling Offered': [31,11,11,11,33,11,21,11,41,11,12,42,21,12,12],
+        'Individual Counseling Offered': [11,33,11,33,33,11,21,11,41,11,12,41,11,11,11],
+        'Group Counseling Offered': [31,11,11,11,33,11,21,11,41,11,12,41,21,43,12],
         'Offered to Employee': [11,11,11,11,33,11,11,11,11,11,11,11,11,11,11],
         "Offered to Employee's Dependents": [12,11,11,11,33,12,12,12,12,12,21,11,11,11,12],
         'Offered to Patients (If Applicable)': [11,11,11,11,33,11,90,90,90,90,90,90,90,90,90],
-        'Available to Community': [13,13,31,94,13,31,13,94,13,21,13,43,13,13,13],
-        'Located On Campus': [11,11,11,11,33,11,31,33,31,11,33,43,11,21,33],
+        'Available to Community': [13,13,31,94,13,31,13,94,13,21,13,41,13,13,13],
+        'Located On Campus': [11,11,11,11,33,11,31,33,31,11,33,41,11,21,33],
         'Located Off-Site (Other Hospitals) (If Applicable)': [11,11,11,11,93,11,90,90,90,90,90,90,90,90,90],
         'Telehealth': [12,33,12,11,33,12,12,12,12,21,12,12,12,12,12],
-        'Communication, Marketing of the Program': [11,41,11,11,33,12,12,11,12,11,12,42,11,12,11]
+        'Communication, Marketing of the Program': [11,41,11,11,33,12,12,11,12,11,12,41,11,12,11]
     }
 }
 
